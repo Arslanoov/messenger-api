00c6f598eb9074dd163b830e21b97d9c9e7a3394
Arslanoov <rasularslanoov@gmail.com>
2021-03-27 16:25:12 +0500

    Implemented hasDialogByMembers method, regenerated api docs

--- app/src/Messenger/Infrastructure/Model/Dialog/DoctrineDialogRepository.php
+++ app/src/Messenger/Infrastructure/Model/Dialog/DoctrineDialogRepository.php
@@ -7,6 +7,7 @@ namespace Messenger\Infrastructure\Model\Dialog;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectRepository;
 use Messenger\Exception\DialogNotFound;
+use Messenger\Model\Author\Author;
 use Messenger\Model\Dialog\Dialog;
 use Messenger\Model\Dialog\DialogRepositoryInterface;
 use Messenger\Model\Dialog\Id;
@@ -39,5 +40,16 @@ final class DoctrineDialogRepository implements DialogRepositoryInterface
         ]);
 
         return $dialog;
+    }
+
+    public function hasDialogByMembers(Author $firstMember, Author $secondMember): bool
+    {
+        return $this->repository->createQueryBuilder('d')
+                ->select('COUNT(d.id)')
+                ->where('d.first_author_id = :first_author_id AND d.second_author_id = :second_author_id')
+                ->orWhere('d.first_author_id = :second_author_id AND d.second_author_id = :first_author_id')
+                ->setParameter(':first_author_id', $firstMember->getUuid()->getValue())
+                ->setParameter(':second_author_id', $secondMember->getUuid()->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
     }
 }
