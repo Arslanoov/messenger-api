0fac5ef45cfca065cf93a5c4c8745cb59a1f9cd7
Arslanoov <rasularslanoov@gmail.com>
2021-06-01 09:39:08 +0500

    feat(user): Added admin create use case

---
+++ app/src/User/UseCase/CreateAdmin/Command.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace User\UseCase\CreateAdmin;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class Command
+{
+    public function __construct(
+        /**
+         * @var string
+         * @Assert\NotBlank()
+         * @Assert\Length(min="4", max="16")
+         */
+        public string $username,
+        /**
+         * @var string
+         * @Assert\NotBlank()
+         * @Assert\Length(min="4", max="16")
+         */
+        public string $password
+    )
+    {
+    }
+}
