b0bec9435c6f69179cd0f1675093b2c58385a147
Arslanoov <rasularslanoov@gmail.com>
2021-06-29 14:59:39 +0500

    fix(dialogs): Removed pagination

--- app/src/Messenger/ReadModel/DialogFetcherInterface.php
+++ app/src/Messenger/ReadModel/DialogFetcherInterface.php
@@ -6,7 +6,7 @@ namespace Messenger\ReadModel;
 
 interface DialogFetcherInterface
 {
-    public function getDialogs(string $authorId, int $page = 1): array;
+    public function getDialogs(string $authorId): array;
 
     public function findDialog(string $dialogId, string $authorId): ?array;
 
